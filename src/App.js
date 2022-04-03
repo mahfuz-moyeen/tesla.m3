57ff48328855895cb89d5b3b7ffa6172a2bfee7f
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-04-04 01:11:26 +0600

    install tailwind

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>welcome my page</h1>
+      <h1 className=' text-red-500'>welcome my page</h1>
     </div>
   );
 }
