b4120a81e87f761834b1de6e553222cbc3693e94
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-04-04 13:01:24 +0600

    update reviews page

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Rating from 'react-rating';
+import { faQuoteLeft, faStar } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Review = ({ review }) => {
     const { name, img, reviews, rate, published } = review
@@ -10,14 +13,19 @@ const Review = ({ review }) => {
                     alt={name}
                     className='w-20 rounded-full absolute -top-5'
                 />
-                <h2 className='mt-14 mb-5 text-lg font-semibold font-mono'>{name}</h2>
+                <h2 className='mt-14 mb-5 text-lg font-semibold font-mono text-orange-500'>{name}</h2>
             </div>
             <div className='flex justify-between my-3'>
-                <p>Published: {published}</p>
-                <p>Rate: {rate}</p>
+                <p className=' text-gray-500'>Published: {published}</p>
+                    <Rating
+                        initialRating={rate}
+                        emptySymbol={<FontAwesomeIcon style={{ color: 'rgb(192, 192, 192)' }} icon={faStar} />}
+                        fullSymbol={<FontAwesomeIcon style={{ color: 'rgb(251 146 60)' }} icon={faStar} />}
+                        readonly
+                    ></Rating>
             </div>
             <div>
-                
+                <FontAwesomeIcon icon={faQuoteLeft}/>
                 <p>{reviews}</p>
             </div>
         </div>
